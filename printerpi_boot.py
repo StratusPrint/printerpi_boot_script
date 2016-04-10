5c08bc36734001ae7a7188cfe4615f89a73083f7
Shawn <smilligan93@gmail.com>
2016-04-10 14:52:29 -0400

    incorporated logger into script.

--- printerpi_boot.py
+++ printerpi_boot.py
@@ -8,6 +8,7 @@ import requests
 import socket
 import json
 import netifaces as ni
+from Logger import Log
 from getpass import getuser
 from wifi import Cell, Scheme
 from uuid import getnode as get_mac
@@ -40,6 +41,7 @@ if __name__ == "__main__":
     uuid_file        = ".uuid"
     api_key          = "THISISNOTAGOODKEY"
     port             = 80
+    print_enabled    = False
 
     if getuser() != 'root':
         print("Must be run as root...")
@@ -61,6 +63,8 @@ if __name__ == "__main__":
     for opt, arg in opts:
         if opt in ["-h", "--help"]:
             print("Usage: run.py -h -s <ssid> -p <password>")
+        elif opt in ["-v", "--verbose"]:
+            print_enabled = True
         elif opt in ["-s", "--ssid"]:
             wifi_ssid = arg
         elif opt in ["-p", "--pass", "--password"]:
@@ -70,10 +74,11 @@ if __name__ == "__main__":
         elif opt in ["-a", "--apikey"]:
             API_KEY = arg
 
+    log = Log(print_enabled=print_enabled)
     if wifi_pass == "":
         print("No password was entered, please specify with '-p <password>'")
         exit(2)
-    print("Connecting to " + wifi_ssid + " using " + wifi_interface)
+    log.log("Connecting to " + wifi_ssid + " using " + wifi_interface)
 
     # Complicated way to account for the file not existing
     try:
@@ -96,12 +101,12 @@ if __name__ == "__main__":
                                          passkey=wifi_pass)
                 scheme.save()
             else:
-                print("SSID " + wifi_ssid + " was not found. Exiting...")
+                log.log("SSID " + wifi_ssid + " was not found. Exiting...")
 
     if scheme.activate() == False:
-        print("Could not connect to " + wifi_ssid + ". Exiting...")
+        log.log("Could not connect to " + wifi_ssid + ". Exiting...")
         exit(3)
-    print("Connected!")
+    log.log("Connected!")
 
     payload = {
             "uuid": get_uuid(uuid_file),
@@ -110,21 +115,23 @@ if __name__ == "__main__":
             "port": str(port)
     }
     url = base_url + printer_activate + "?payload=" + json.dumps(payload)
-    print("Activating printer, watch output of details")
+    log.log("Activating printer, watch output of details")
     for i in range(100):
         try:
             r = requests.get(url, timeout=1)
             if r.status_code == requests.codes.ok:
-                print("Response was okay! Good to go. Exiting...")
+                log.log("Response was okay! Good to go. Exiting...")
                 exit(0)
             else:
                 #TODO make this more descriptive
-                print("Response was " + r.status_code
+                log.log("Response was " + r.status_code
                         + ". Something went wrong")
                 exit(4)
         except requests.ConnectionError:
-            print("Error occured, will try " + str(100 - i) + " more times.")
+            if i % 5 == 0:
+                log.log("Error occured, will try "
+                        + str(100 - i) + " more times.")
             sleep(1)
-    print("No connection after 100 attempts. Call the ghostbusters")
+    log.log("No connection after 100 attempts. Call the ghostbusters")
     exit(5)
 
